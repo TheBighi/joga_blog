08c8a0acb05d2da09dc12fa44b9d524edb5b462a
TheBighi <thebighix@gmail.com>
2025-11-21 18:02:28 +0200

    added basics

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const Express = require("express")
 const mongodb = require('mongodb').MongoClient
-
+const dotenv = require("dotenv")
 const app = Express()
+
+dotenv.config()
 
 connectToDB = async (connectionString) => {
     try{
         const client = await mongodb.connect(connectionString)
         db = client.db()
         console.log("Connected to mongoDB")
+        return db
     }
     catch(err){
         console.log(err)
     }
-    return db
 }
-let db, connectionString = 'mongodb://joga_admin:qwerty@localhost:27017/joga_blog'
+
+const user = process.env.MONGO_USER
+const password = process.env.MONGO_PASSWORD
+const host = process.env.MONGO_HOST
+const port = process.env.MONGO_PORT
+const dbname = process.env.MONGO_DATABASE
+
+let db, connectionString = `mongodb://${user}:${password}@${host}:${port}/`
 
 db = connectToDB(connectionString)
 
-app.listen(3001, () => {
-    console.log("On localhost:3001")
+app.listen(port, () => {
+    console.log(`On https://localhost:${port}`)
 })
